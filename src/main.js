f825ac4d8cbf80060f9931611b788d70d341098f
Alex And <alexandr_works@ukr.net>
2021-01-27 19:28:45 +0200

    removed vuex, search.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import '@babel/polyfill'
 import 'mutationobserver-shim'
 import Vue from 'vue'
 import App from './App.vue'
-import store from './store'
 
 import './plugins/bootstrap-vue'
 import './plugins/instant-search'
@@ -11,6 +10,5 @@ import './plugins/instant-search'
 Vue.config.productionTip = false
 
 new Vue({
-  store,
   render: h => h(App)
 }).$mount('#app')
